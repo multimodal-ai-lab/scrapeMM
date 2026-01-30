f9d8fc6120ac24a761581168e447270ad10c3504
Flo <fschroet@students.uni-mainz.de>
2026-01-30 01:55:04 +0100

    remove view port specifications

--- scrapemm/integrations/archive_today.py
+++ scrapemm/integrations/archive_today.py
@@ -42,7 +42,6 @@ class ArchiveToday(RetrievalIntegration):
             browser = await p.chromium.launch(headless=True)
             context = await browser.new_context(
                 user_agent=HEADERS["User-Agent"],
-                viewport={"width": 1920, "height": 1080},
             )
             page = await context.new_page()
 
